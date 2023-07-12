00f892ebf4659b02e1fc3223a09fa119c1b67e10
ImGunHyoeng <ynoe30@gmail.com>
2023-07-12 17:42:01 +0900

    add cpp and do homework

--- Beluga/Source/Beluga/Beluga.cpp
+++ Beluga/Source/Beluga/Beluga.cpp
@@ -4,9 +4,12 @@ DEFINE_LOG_CATEGORY(LogBeluga);
 class FBelugaModule : public FDefaultGameModuleImpl
 {
 public:
-	virtual void StartupModule() override
+	virtual void StartupModule() override//dll이 로딩되면 불러와짐
 	{
 		UE_LOG(LogBeluga, Display, TEXT("BelugaModule::StartupModule"));
+
+		extern void StringTest();//링크하면 언젠가 있다. 원래는 헤더파일로 하는것
+		StringTest();//서로다른 번역단위간의 정보는 알수 없다 헤더파일로 심볼을 넘겨서 알수 있다.
 	}
 	virtual void ShutdownModule() override
 	{
---
+++ Beluga/Source/Beluga/StringTest.cpp
@@ -0,0 +1,52 @@
+#include "BelugaMinimal.h"
+
+void StringTest()
+{
+	//생성
+		//char	"asdf"
+		//wchar	L"asdf"
+		// tchar TEXT("asdf")
+		// 
+		// 
+
+	//대입 연산자 =
+
+	//printf
+
+	//Format
+		//
+
+	//operator []
+
+	//대소 비교
+
+	//TCHAR* 얻기
+	//operator *
+
+	//길이구하기
+
+	//Replace
+
+	//insert
+
+	//clear
+
+	//split
+	
+	//IsEmpty
+
+	//Shrink
+
+	//RemoveAt
+
+	//find 로 인덱스
+
+	
+	//Left
+	//Mid
+	//Right
+
+	//대문자로 변환,소문자로 변환
+	
+	//
+}
